0e94933292a555a91a01cb18ee7df8351416e752
hui3694 <1054541133@qq.com>
2018-05-12 02:06:41 +0800

    11

---
+++ mvc/Areas/SPA/Controllers/SpaBulkUploadController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using mvc.Filters;
+using System.Threading;
+using System.Threading.Tasks;
+using mvc.ViewModels;
+using BusinessEntities;
+using System.IO;
+using BusinessLayer;
+
+namespace mvc.Areas.SPA.Controllers
+{
+    public class SpaBulkUploadController : AsyncController
+    {
+        // GET: SPA/SpaBulkUpload
+
+        [AdminFilter]
+        public ActionResult Index()
+        {
+            return PartialView("index");
+            //return View();
+        }
+         
+        [AdminFilter]
+        public async Task<ActionResult> Upload(FileUploadViewModel model)
+        {
+            int t1 = Thread.CurrentThread.ManagedThreadId;
+            List<Employee> employees = await Task.Factory.StartNew<List<Employee>>
+                (() => GetEmployee(model));
+            int t2 = Thread.CurrentThread.ManagedThreadId;
+            EmployeeBusinessLayer bal = new EmployeeBusinessLayer();
+            bal.UploadEmployees(employees);
+            EmployeeListViewModel vm = new EmployeeListViewModel();
+            vm.Employees = new List<EmployeeViewModel>();
+            foreach(Employee item in employees)
+            {
+                EmployeeViewModel evm = new EmployeeViewModel();
+                evm.EmployeeName = item.FirstName + " " + item.LastName;
+                evm.Salary = item.Salary.Value.ToString("C");
+                if (item.Salary > 15000)
+                {
+                    evm.SalaryColor = "yellow";
+                }
+                else
+                {
+                    evm.SalaryColor = "green";
+                }
+                vm.Employees.Add(evm);
+            }
+            return Json(vm);
+        }
+
+        private List<Employee> GetEmployee(FileUploadViewModel model)
+        {
+            List<Employee> employees = new List<Employee>();
+            StreamReader csvreader = new StreamReader(model.fileUpload.InputStream);
+            csvreader.ReadLine();
+            while (!csvreader.EndOfStream)
+            {
+                var line = csvreader.ReadLine();
+                var values = line.Split(',');
+                Employee e = new Employee();
+                e.FirstName = values[0];
+                e.LastName = values[1];
+                e.Salary = int.Parse(values[2]);
+                employees.Add(e);
+            }
+            return employees;
+        }
+
+    }
+}
